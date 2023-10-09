97bcca65380d1369f98a461b26214fe51bcd648a
chamm99 <alslalsl3125@gmail.com>
2023-10-10 00:49:36 +0900

    h2db

--- src/main/java/beginner/core/SpringConfig.java
+++ src/main/java/beginner/core/SpringConfig.java
@@ -1,20 +1,29 @@
 package beginner.core;
 
+import beginner.core.repository.JdbcMemberRepository;
 import beginner.core.repository.MemberRepository;
 import beginner.core.repository.MemoryMemberRepository;
 import beginner.core.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
-
+    private DataSource dataSource;
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
     }
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+    //    return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
