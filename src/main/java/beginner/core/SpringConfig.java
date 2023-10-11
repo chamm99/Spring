adabb9890f7d1f8dc4bfeb957cc72d0aeca6c181
chamm99 <alslalsl3125@gmail.com>
2023-10-12 00:06:18 +0900

    JPA2

--- src/main/java/beginner/core/SpringConfig.java
+++ src/main/java/beginner/core/SpringConfig.java
@@ -2,22 +2,25 @@ package beginner.core;
 
 import beginner.core.repository.*;
 import beginner.core.service.MemberService;
-import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.sql.DataSource;
-
 @Configuration
 public class SpringConfig {
 
-    private EntityManager em;
-
+    private final MemberRepository memberRepository;
     @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
+
+//    private EntityManager em;
+
+//    @Autowired
+//    public SpringConfig(EntityManager em) {
+//        this.em = em;
+//    }
 
 //    private DataSource dataSource;
 //    @Autowired
@@ -26,13 +29,13 @@ public class SpringConfig {
 //    }
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
-    @Bean
-    public MemberRepository memberRepository() {
+//    @Bean
+//    public MemberRepository memberRepository() {
     //    return new MemoryMemberRepository();
     //    return new JdbcMemberRepository(dataSource);
     //    return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+    //    return new JpaMemberRepository(em);
+//    }
 }
---
+++ src/main/java/beginner/core/repository/SpringDataJpaMemberRepository.java
@@ -0,0 +1,12 @@
+package beginner.core.repository;
+
+import beginner.core.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
+    //JPOL select m from Member m where m.name = ?
+    @Override
+    Optional<Member> findByName(String name);
+}
